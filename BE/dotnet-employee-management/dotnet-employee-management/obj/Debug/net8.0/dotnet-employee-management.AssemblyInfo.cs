3a31b743ab3568e13293e8e34b1d9df75920351c
QUIGAS\gairo <gaironmangas@gmail.com>
2024-11-29 10:14:41 +0700

    Add Libs

--- BE/dotnet-employee-management/dotnet-employee-management/obj/Debug/net8.0/dotnet-employee-management.AssemblyInfo.cs
+++ BE/dotnet-employee-management/dotnet-employee-management/obj/Debug/net8.0/dotnet-employee-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-employee-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df1d7cc1e9be1324e1b306c84404b0ff0ab7d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-employee-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-employee-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
